a2dceb94441517a3d8ea8f4397b30b3f6909965c
kazisamin <azharul.samin@gmail.com>
2023-08-18 23:28:42 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,46 @@
+/* eslint-disable no-console */
+import { Server } from 'http';
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config/index';
+import { errorlogger, logger } from './shared/logger';
+
+process.on('uncaughtException', error => {
+  errorlogger.error(error);
+  process.exit(1);
+});
+
+let server: Server;
+
+async function bootstrap() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log(`👍  Database Is Connected Successfully`);
+
+    server = app.listen(config.port, () => {
+      console.log(`➡️   Application  Listening On Port ${config.port}`);
+    });
+  } catch (err) {
+    errorlogger.error('❌ Failed to Connect Database', err);
+  }
+
+  process.on('unhandledRejection', error => {
+    if (server) {
+      server.close(() => {
+        errorlogger.error(error);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
+}
+
+bootstrap();
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM is received');
+  if (server) {
+    server.close();
+  }
+});
